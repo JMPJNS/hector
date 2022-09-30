a152d75a379fd0387d79f5f9ded0acf025768523
Jonas <jonas@jmp.blue>
2022-09-30 13:27:30 +0200

    base entity

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -1,14 +1,16 @@
 
-import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+import { BaseEntity, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
 import { DeepPartial } from "../types/types.js"
 
-export abstract class HectorEntity {
+export abstract class HectorEntity extends BaseEntity {
     protected constructor(input?: DeepPartial<HectorEntity>) {
-        if (input) {
-            for (const [key, value] of Object.entries(input)) {
-                (this as any)[key] = value
-            }
-        }
+			super()
+			if (input) {
+					for (const [key, value] of Object.entries(input)) {
+							(this as any)[key] = value
+					}
+			}
+			console.log(this)
     }
 
     @PrimaryGeneratedColumn()
